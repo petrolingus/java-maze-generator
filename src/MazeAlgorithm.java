a5265a5541f2150f683b168d0044cf89fd648cb5
Aleksandr Lebedev <aleksandr.a.lebedev@netcracker.com>
2021-09-21 17:14:42 +0300

    Performance fix

--- src/MazeAlgorithm.java
+++ src/MazeAlgorithm.java
@@ -6,6 +6,7 @@ public class MazeAlgorithm {
 
     public static void main(String[] args) {
 
+//        RandomGenerator randomGenerator = RandomGenerator.of("L32X64MixRandom");
         RandomGenerator randomGenerator = RandomGenerator.of("SplittableRandom");
 //        RandomGenerator randomGenerator = RandomGenerator.getDefault();
 
@@ -32,8 +33,32 @@ public class MazeAlgorithm {
             int lock = 0;
 
             while (true) {
-                int direction = randomGenerator.nextInt(4);
-                randoms++;
+                int bit0 = lock & 1;
+                int bit1 = (lock & 2) >> 1;
+                int bit2 = (lock & 4) >> 2;
+                int bit3 = (lock & 8) >> 3;
+                int free = bit0 + bit1 + bit2 + bit3; // occupied bytes
+                int direction;
+                if (free == 3) {
+//                    System.out.println("BITS:");
+//                    System.out.println(bit0);
+//                    System.out.println(bit1);
+//                    System.out.println(bit2);
+//                    System.out.println(bit3);
+//                    System.out.println("######");
+                    if (bit0 == 0) {
+                        direction = 0;
+                    } else if (bit1 == 0) {
+                        direction = 1;
+                    } else if (bit2 == 0) {
+                        direction = 2;
+                    } else {
+                        direction = 3;
+                    }
+                } else {
+                    direction = randomGenerator.nextInt(4);
+                    randoms++;
+                }
                 if (direction == 0 && cy != 0 && maze[cy - 1][cx] == 0) {
                     maze[cy][cx] |= 1;
                     cy--;
@@ -86,9 +111,13 @@ public class MazeAlgorithm {
         long stop = System.nanoTime();
         System.out.println(Duration.ofNanos(stop - start).toMillis() + " ms");
 
-        System.out.println("randoms:" + randoms);
+        System.out.println("random calls:" + randoms);
         // 16777216
         // 178460734
+        // 178466550
+        // 178487416
+        // 178483899
+        // 106591713
 
 //        System.out.println();
 //        for (int i = 0; i < size; i++) {
