05d0b8bd6b9215ad1045767b5119a49660179636
Aleksandr Lebedev <aleksandr.a.lebedev@netcracker.com>
2021-09-21 05:28:42 +0300

    Created working maze algorithm

--- src/MazeAlgorithm.java
+++ src/MazeAlgorithm.java
@@ -1,3 +1,4 @@
+import java.time.Duration;
 import java.util.Arrays;
 import java.util.random.RandomGenerator;
 
@@ -6,71 +7,94 @@ public class MazeAlgorithm {
     public static void main(String[] args) {
 
         RandomGenerator randomGenerator = RandomGenerator.of("SplittableRandom");
+//        RandomGenerator randomGenerator = RandomGenerator.getDefault();
 
-        int size = 5;
+        int randoms = 0;
+
+        int size = 4096;
 
         int[][] maze = new int[size][size];
+        maze[0][0] = 0;
 
-        int[][] visited = new int[size][size];
-        for (int i = 0; i < size; i++) {
-            for (int j = 0; j < size; j++) {
-                visited[i][j] = 0;
-            }
-        }
+        int cx = 0;
+        int cy = 0;
 
-        int current = 0;
-        int[] xs = new int[size];
-        int[] ys = new int[size];
-        xs[current] = 0;
-        ys[current] = 0;
+        // STACK
+        int pointer = 0;
+        int[] xs = new int[size * size];
+        int[] ys = new int[size * size];
 
-        // Set bounds
-        maze[0][0] = 9;
-        maze[0][size - 1] = 5;
-        maze[size - 1][size - 1] = 6;
-        maze[size - 1][0] = 10;
-        for (int i = 1; i < size - 1; i++) {
-            maze[i][0] = 8; // left
-            maze[i][size - 1] = 4; // right
-            maze[0][i] = 1; //top
-            maze[size - 1][i] = 2; // bottom
-        }
+        long start = System.nanoTime();
 
         // Creation maze
-        for (int i = 0; i < 10; i++) {
+        do {
 
-            int direction = 1 << randomGenerator.nextInt(4);
-            int wall = maze[xs[current]][ys[current]];
-            if (wall == 15) {
-                current--;
-                continue;
+            int lock = 0;
+
+            while (true) {
+                int direction = randomGenerator.nextInt(4);
+                randoms++;
+                if (direction == 0 && cy != 0 && maze[cy - 1][cx] == 0) {
+                    maze[cy][cx] |= 1;
+                    cy--;
+                    maze[cy][cx] |= 2;
+                    xs[pointer] = cx;
+                    ys[pointer] = cy;
+                    pointer++;
+//                    System.out.print("dir=" + (direction) + "(top) ");
+                    break;
+                } else if (direction == 1 && (cy != size - 1) && maze[cy + 1][cx] == 0) {
+                    maze[cy][cx] |= 2;
+                    cy++;
+                    maze[cy][cx] |= 1;
+                    xs[pointer] = cx;
+                    ys[pointer] = cy;
+                    pointer++;
+//                    System.out.print("dir=" + (direction) + "(bot) ");
+                    break;
+                } else if (direction == 2 && (cx != size - 1) && maze[cy][cx + 1] == 0) {
+                    maze[cy][cx] |= 4;
+                    cx++;
+                    maze[cy][cx] |= 8;
+                    xs[pointer] = cx;
+                    ys[pointer] = cy;
+                    pointer++;
+//                    System.out.print("dir=" + (direction) + "(right) ");
+                    break;
+                } else if (direction == 3 && cx != 0 && maze[cy][cx - 1] == 0) {
+                    maze[cy][cx] |= 8;
+                    cx--;
+                    maze[cy][cx] |= 4;
+                    xs[pointer] = cx;
+                    ys[pointer] = cy;
+                    pointer++;
+//                    System.out.print("dir=" + (direction) + "(left) ");
+                    break;
+                }
+                lock |= 1 << direction;
+                if (lock == 15) {
+                    pointer--;
+                    cx = xs[pointer];
+                    cy = ys[pointer];
+//                    System.out.print("break ");
+                    break;
+                }
             }
-            while ((direction & wall) == 1) {
-                direction = 1 << randomGenerator.nextInt(4);
-            }
 
-            maze[xs[current]][ys[current]] |= direction;
-            current++;
+        } while (pointer != 0);
 
-            if (direction == 1) {
-                ys[current] = ys[current - 1] - 1;
-                maze[xs[current]][ys[current]] |= direction << 1;
-            } else if (direction == 2) {
-                ys[current] = ys[current - 1] + 1;
-                maze[xs[current]][ys[current]] |= direction >> 1;
-            } else if (direction == 4) {
-                xs[current] = xs[current - 1] + 1;
-                maze[xs[current]][ys[current]] |= direction << 1;
-            } else {
-                xs[current] = xs[current - 1] - 1;
-                maze[xs[current]][ys[current]] |= direction >> 1;
-            }
+        long stop = System.nanoTime();
+        System.out.println(Duration.ofNanos(stop - start).toMillis() + " ms");
 
-        }
+        System.out.println("randoms:" + randoms);
+        // 16777216
+        // 178460734
 
-        for (int i = 0; i < size; i++) {
-            System.out.println(Arrays.toString(maze[i]));
-        }
+//        System.out.println();
+//        for (int i = 0; i < size; i++) {
+//            System.out.println(Arrays.toString(maze[i]));
+//        }
 
     }
+
 }
