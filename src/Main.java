a648a93d09d5a778ae87dfdd29f4a860fb219f72
Aleksandr Lebedev <aleksandr.a.lebedev@netcracker.com>
2021-09-23 15:40:49 +0300

    Added new realization in Main class

--- src/Main.java
+++ src/Main.java
@@ -1,58 +1,51 @@
+import image.BMP;
+
+import java.io.IOException;
 import java.util.Arrays;
-import java.util.Random;
+import java.util.random.RandomGenerator;
 
 public class Main {
 
     public static void main(String[] args) {
 
+        RandomGenerator randomGenerate = RandomGenerator.of("SplittableRandom");
+
         int size = 5;
 
         int[][] maze = new int[size][size];
 
-        int cx = 0;
-        int cy = 0;
-
-        Random random = new Random();
-
-        long start = System.nanoTime();
-
-
-        // Set valid bounds
-        maze[0][0] = 9 << 4;
-        maze[0][size - 1] = 5 << 4;
-        maze[size - 1][size - 1] = 6 << 4;
-        maze[size - 1][0] = 10 << 4;
-        for (int i = 1; i < size - 1; i++) {
-            maze[i][0] = 8 << 4; // left
-            maze[i][size - 1] = 4 << 4; // right
-            maze[0][i] = 1 << 4; //top
-            maze[size - 1][i] = 2 << 4; // bottom
+        for (int i = 0; i < size; i++) {
+            maze[0][i] = 1;
+            maze[size - 1][i] = 1;
+            maze[i][0] = 1;
+            maze[i][size - 1] = 1;
         }
 
-        for (int i = 0; i < 1000; i++) {
+        int cx = 1;
+        int cy = 1;
 
-            int dir = 1 << random.nextInt(4);
-            int dx = 0;
-            int dy = 0;
+        while (true) {
 
-            if (dir == 1) {
-                dy = -1;
-            } else if (dir == 2) {
-                dy = 1;
-            } else if (dir == 4) {
-                dx = 1;
-            } else {
-                dx = -1;
+            int up = maze[cy - 1][cx];
+            int down = maze[cy + 1][cx];
+            int right = maze[cy][cx + 1];
+            int left = maze[cy][cx - 1];
+
+            if ((up + down + right + left) == 4) {
+                break;
             }
 
-            int nx = cx + dx;
-            int ny = cy + dy;
+            int[] neighbours = {up, down, right, left};
 
-            if ((nx >= 0 && nx < size) && (ny >= 0 && ny < size) && (maze[ny][nx] == 0) ) {
-                maze[cy][cx] = dir;
-                cx = nx;
-                cy = ny;
+            int index = randomGenerate.nextInt(4);
+            int direction = neighbours[index];
+            while (direction == 1) {
+                direction = randomGenerate.nextInt(4); // TODO: think about this, we can use increment
             }
+
+            cx = cx + 1;
+            cy = cy + 1;
+
 
 
         }
@@ -61,8 +54,53 @@ public class Main {
             System.out.println(Arrays.toString(maze[i]));
         }
 
-        long stop = System.nanoTime() - start;
-        System.out.println((stop / 1_000_000) + " ms");
+    }
+
+    private static void generateImage(int[][] maze, String name) {
+
+        int size = maze.length;
+        int imageSize = size * 4;
+        int[][] rgbValues = new int[imageSize][imageSize];
+
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+
+                int code = maze[i][j];
+                int up = code & 1;
+                int bot = code & 2;
+                int right = code & 4;
+                int left = code & 8;
+                int x = 4 * j;
+                int y = 4 * i;
+                rgbValues[y + 1][x + 1] = 33554431;
+                rgbValues[y + 2][x + 2] = 33554431;
+                rgbValues[y + 1][x + 2] = 33554431;
+                rgbValues[y + 2][x + 1] = 33554431;
+                if (up != 0) {
+                    rgbValues[y][x + 1] = 33554431;
+                    rgbValues[y][x + 2] = 33554431;
+                }
+                if (bot != 0) {
+                    rgbValues[y + 3][x + 1] = 33554431;
+                    rgbValues[y + 3][x + 2] = 33554431;
+                }
+                if (right != 0) {
+                    rgbValues[y + 1][x + 3] = 33554431;
+                    rgbValues[y + 2][x + 3] = 33554431;
+                }
+                if (left != 0) {
+                    rgbValues[y + 1][x] = 33554431;
+                    rgbValues[y + 2][x] = 33554431;
+                }
+            }
+        }
+        BMP bmp = new BMP();
+        try {
+            bmp.saveBMP(name + ".bmp", rgbValues);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
     }
+
 }
